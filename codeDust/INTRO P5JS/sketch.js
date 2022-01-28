66b6266fbd66437261ec8a30714253604b678e98
ekramyousif <90456883+ekramyousif@users.noreply.github.com>
2022-01-28 12:24:24 +0000

    Add files via upload

---
+++ codeDust/INTRO P5JS/sketch.js
@@ -0,0 +1,13 @@
+//qustion:1
+//Try changing the number on the "createcanva"and "background"
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220, 55, 150);
+  triangle(30, 30, 180, 130 ,250, 50);
+  fill(78,66,22);
+  
+  
+  }
